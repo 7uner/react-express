ed9aa27d2709e4a4608e7a809f7172499b2d1cf2
Winston Zhu <winston.zhu@yahoo.ca>
2024-03-25 19:32:29 -0400

    added javascript examples

--- src/app/components/NavBar/NavBar.jsx
+++ src/app/components/NavBar/NavBar.jsx
@@ -69,18 +69,26 @@ function NavBar() {
                 Disabled
               </a>
             </li>
+
+            <form
+              className="d-flex ms-5"
+              style={{ width: '1000px' }}
+              role="search"
+            >
+              <input
+                className="form-control"
+                type="search"
+                placeholder="Search Product"
+                aria-label="Search"
+              />
+              <button className="btn btn-outline-success" type="submit">
+                Search
+              </button>
+            </form>
           </ul>
-          <form className="d-flex" role="search">
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button className="btn btn-outline-success" type="submit">
-              Search
-            </button>
-          </form>
+          <a className="btn btn-outline-success" href="/cart">
+            Cart
+          </a>
         </div>
       </div>
     </nav>
