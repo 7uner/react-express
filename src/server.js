d2f077c45169caf6c44b6259284fa009fc2ce990
Winston Zhu <winston.zhu@yahoo.ca>
2024-06-03 20:54:57 -0400

    June 3rd Server examples

--- src/server.js
+++ src/server.js
@@ -10,14 +10,41 @@ const app = express();
 app.use(express.json());
 
 // Serve API requests from the router
-app.use('/api', router);
+//app.use('/api', router);
 
 // Serve app production bundle
 app.use(express.static('dist/app'));
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers'
+  );
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Private-Network', true);
+  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader('Access-Control-Max-Age', 7200);
+
+  next();
+});
+
 // Handle client routing, return all requests to the app
 app.get('/', (_req, res) => {
   res.sendFile(path.join(__dirname, 'app/index.html'));
+});
+
+app.get('/hello', (_req, res) => {
+  res.json({ Message: 'hello world' });
+});
+
+app.post('/formSubmit', (_req, res) => {
+  console.log(_req.body);
+  res.status(200).json({ Message: 'form data revieved and processed' });
 });
 
 app.listen(PORT, () => {
