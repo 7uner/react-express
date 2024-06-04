e0c8e63bb3c6de20de66c4169ae7b1cf924bde99
ExpressoDepresso <jason.rj.liu@gmail.com>
2024-06-03 21:30:17 -0400

    Feedback
    Feedback Form + About

--- src/app/pages/About.jsx
+++ src/app/pages/About.jsx
@@ -1,10 +1,68 @@
 import NavBar from '../components/NavBar/NavBar';
 import Footer from '../components/NavBar/Footer';
+import { useState } from 'react';
+
 function About() {
+  const [message, setMessage] = useState('No response recieved yet');
+  const [respose, setResponse] = useState('No response recieved yet');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+
+  function getMessage() {
+    fetch('http://localhost:3001/hello')
+      .then((response) => response.json())
+      .then((json) => setMessage(json.Message))
+      .catch((error) => console.error(error));
+  }
+
+  function sendData(e) {
+    e.preventDefault();
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ firstName: firstName, lastName: lastName }),
+    };
+    fetch('http://localhost:3001/formSubmit', requestOptions)
+      .then((response) => response.json())
+      .then((json) => setResponse(json.Message))
+      .catch((error) => console.error(error));
+  }
+
   return (
     <div>
       <NavBar />
-      <div>About</div>
+      <div>Press the button to get a greeting from the backend server:</div>
+      <a className="btn btn-primary" onClick={getMessage}>
+        Get Request
+      </a>
+      <div>{message}</div>
+      <div>
+        Use the form below to send some form data to the back end server
+      </div>
+      <form>
+        <label htmlFor="fname">First name:</label>
+        <br />
+        <input
+          type="text"
+          id="fname"
+          name="fname"
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+        <br />
+        <label htmlFor="lname">Last name:</label>
+        <br />
+        <input
+          type="text"
+          id="lname"
+          name="lname"
+          onChange={(e) => setLastName(e.target.value)}
+        />
+        <br />
+        <button className="btn btn-primary" onClick={sendData}>
+          Send Data
+        </button>
+      </form>
+      <div>{respose}</div>
       <Footer />
     </div>
   );
